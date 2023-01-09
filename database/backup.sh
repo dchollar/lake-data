12cdfc2c7eeb733d8a33048e47e77e6000bc469d
Dan Hollar <daniel.hollar@gmail.com>
2023-01-09 05:56:01 +0000

    fixing backup for new machine

--- database/backup.sh
+++ database/backup.sh
@@ -1,26 +1,30 @@
 #!/usr/bin/env sh
 
-projectFolder=/home/dan/IdeaProjects/lake-data
+projectFolder=/home/dan/work/lake-data
 # Backup storage directory
 backupFolder=$projectFolder/database
 
 # MySQL user
 user=backup
 
-sqlFile=$backupFolder/large_dump.sql
-zipFile=$backupFolder/large_dump.zip
+fileName=large_dump
+sqlFile=$backupFolder/$fileName.sql
+zipFile=$fileName.zip
 #this file holds the password and is not checked in
 defaultFile=$backupFolder/defaults.cnf
 
-cd $projectFolder
+echo "performing backup"
+cd $projectFolder || exit
 mysqldump --defaults-file=$defaultFile --databases pipe_lake --user=$user --lock-tables --add-drop-database --result-file=$sqlFile
 
-# Compress backup
+echo "compressing backup"
+cd $backupFolder || exit
 rm $zipFile
-zip -9 -q $zipFile $sqlFile
-rm $sqlFile
+zip -9 -q $zipFile $fileName.sql
+rm $fileName.sql
+cd $projectFolder || exit
 
+echo "pushing to git"
 git add database/large_dump.zip
 git commit -m "database backup"
 git push
-
