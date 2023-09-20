126f3715869839ec4e34c365b6ff6c6ff17e3f5a
Dan Hollar <daniel.hollar@gmail.com>
2023-09-19 23:25:19 -0500

    fixing security configuration issue

--- src/main/groovy/com/lake/WebSecurityConfig.groovy
+++ src/main/groovy/com/lake/WebSecurityConfig.groovy
@@ -4,6 +4,7 @@ import com.lake.service.ReporterService
 import groovy.transform.CompileStatic
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.authentication.AuthenticationProvider
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.Customizer
@@ -11,6 +12,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector
 
 @CompileStatic
 @Configuration
@@ -24,6 +27,11 @@ class WebSecurityConfig {
     }
 
     @Bean
+    MvcRequestMatcher.Builder mvc(HandlerMappingIntrospector introspector) {
+        return new MvcRequestMatcher.Builder(introspector);
+    }
+
+    @Bean
     AuthenticationProvider authenticationProvider(ReporterService reporterService, PasswordEncoder passwordEncoder) {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider()
         provider.setUserDetailsService(reporterService)
@@ -32,11 +40,16 @@ class WebSecurityConfig {
     }
 
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) {
+    SecurityFilterChain securityFilterChain(HttpSecurity http, MvcRequestMatcher.Builder mvc) {
         http.csrf(csrf -> csrf.disable()).headers {
             it.frameOptions { it.disable() }
         }.authorizeHttpRequests {
-            it.requestMatchers('/', '/index', '/home', '/public/**', '/favicon.ico').permitAll().anyRequest().authenticated()
+            //it.requestMatchers('/', '/index', '/home', '/public/**', '/favicon.ico').permitAll().anyRequest().authenticated()
+            it.requestMatchers(mvc.pattern(HttpMethod.GET, '/')).permitAll()
+            it.requestMatchers(mvc.pattern(HttpMethod.GET, '/home')).permitAll()
+            it.requestMatchers(mvc.pattern(HttpMethod.GET, '/favicon.ico')).permitAll()
+            it.requestMatchers(mvc.pattern( '/public/**')).permitAll()
+            it.anyRequest().authenticated()
         }.formLogin {
             it.permitAll()
         }.logout {
